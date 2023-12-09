6ff288b22a7e98b75639e291acff70c05a7affe9
omer-yakici <116022575+omer-yakici@users.noreply.github.com>
2023-12-09 13:30:07 +0300

    Add files via upload

---
+++ Rotating Cube2/Rotating Cube2/Source.cpp
@@ -0,0 +1,37 @@
+﻿#include <SFML/Graphics.hpp>
+
+int main() {
+    // Pencere oluştur
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Cube Animation");
+
+    // Küp özellikleri
+    sf::Vector2f cubeSize(100.f, 100.f);
+    sf::RectangleShape cube(cubeSize);
+    cube.setFillColor(sf::Color::Blue);
+    cube.setOrigin(cubeSize.x / 2.f, cubeSize.y / 2.f);
+    cube.setPosition(window.getSize().x / 2.f, window.getSize().y / 2.f);
+
+    // Ana döngü
+    while (window.isOpen()) {
+        // Olayları işle
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        // Küpü döndür
+        cube.rotate(0.1f);  // Küpün dönme hızını ayarla
+
+        // Ekranı temizle (arka planı beyaza ayarla)
+        window.clear(sf::Color::White);
+
+        // Küpü çiz
+        window.draw(cube);
+
+        // Ekranı göster
+        window.display();
+    }
+
+    return 0;
+}
